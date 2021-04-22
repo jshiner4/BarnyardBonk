138d8d7343b87923d84958cc1a4ba599da2dd108
Jessalyn Shiner <59877841+jshiner4@users.noreply.github.com>
2021-04-22 17:12:27 -0400

    updated again

---
+++ Barnyard Bonk/Assets/Edited scripts/New Folder/ProjectileBaseNew.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ProjectileBaseNew : MonoBehaviour
+{
+    public GameObject owner { get; private set; }
+    public Vector3 initialPosition { get; private set; }
+    public Vector3 initialDirection { get; private set; }
+    public Vector3 inheritedMuzzleVelocity { get; private set; }
+    public float initialCharge { get; private set; }
+
+    public UnityAction onShoot;
+
+    public void Shoot(WeaponControllerNew controller)
+    {
+        owner = controller.owner;
+        initialPosition = transform.position;
+        initialDirection = transform.forward;
+        inheritedMuzzleVelocity = controller.muzzleWorldVelocity;
+        initialCharge = controller.currentCharge;
+
+        if (onShoot != null)
+        {
+            onShoot.Invoke();
+        }
+    }
+}
---
+++ Barnyard Bonk/Assets/Edited scripts/RayShooter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour
+{
+    private Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 point = new Vector3(camera.pixelWidth / 2, camera.pixelHeight / 2);
+            Ray ray = camera.ScreenPointToRay(point);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("hit " + hit.point);
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        int size = 12;
+        float posX = camera.pixelWidth / 2 - size / 4;
+        float posY = camera.pixelHeight / 2 - size / 2;
+        GUI.Label(new Rect(posX, posY, size, size), "+");
+    }
+}
